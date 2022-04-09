a12e92a55d421f21704c31da66f289a47582f68a
Emin91 <emin.proger@gmail.com>
2022-04-10 00:54:53 +0400

    dfgsdf

--- index.js
+++ index.js
@@ -16,7 +16,25 @@ async function wait(ms) {
     });
 }
 
+async function getParse() {
+    request('https://www.ivi.az/collections/best-movies', (error, response, html) => {
+    if (!error && response.statusCode == 200) {
+        const $ = cheerio.load(html);
+
+        // Get best movies list
+        $('.gallery__item.gallery__item_virtual').each((i, el) => {
+            const title = $(el).find('.nbl-slimPosterBlock__title').text() || null;
+            const imgLink = $(el).find('img').attr('src') || null;
+            const movieId = $(el).find('a').attr('href').match(/\d+/g).toString() || null;
+            const ageLimit = $(el).find('.nbl-poster__nbl-ageBadge').attr('class').match(/\d+/g).toString() || null;
+            arr.push({ id: uuid(), title, imgLink, movieId, ageLimit });
+        })
+    }
+})
+}
+
 let mypromise = function functionOne(testInput) {
+    getParse()
     return new Promise((resolve, reject) => {
         setTimeout(
             () => {
