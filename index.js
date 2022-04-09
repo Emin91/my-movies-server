c7013dba2175d8b39fc7321f054640435c6f4d56
Emin91 <emin.proger@gmail.com>
2022-04-10 01:14:12 +0400

    sfdsd

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.get('/', async (req, res) => {
         arr.push({ id: uuid(), title, imgLink, movieId, ageLimit });
     })
     await wait(6000)
-    arr.length && res.json({data: JSON.stringify(arr)})
+    res.json({data: JSON.stringify(arr)})
 });
 
 
