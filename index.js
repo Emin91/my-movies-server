2d9e1ea76c820e26e066f1faa811fcffe73ea005
Emin91 <emin.proger@gmail.com>
2022-04-10 00:21:30 +0400

    test

--- index.js
+++ index.js
@@ -26,11 +26,11 @@ function callParse() {
     })
 
 }
-// async function wait(ms) {
-//     return new Promise((resolve, reject) => {
-//         setTimeout(resolve, ms)
-//     });
-// }
+async function wait(ms) {
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, ms)
+    });
+}
 
 app.get('/movie/:id', (req, res) => {
     const qualityList = [];
@@ -97,6 +97,7 @@ app.get('/movie/:id', (req, res) => {
 
 app.get('/', async (req, res) => {
     callParse()
+    await wait(6000)
     res.json({ data: 'JSON.stringify(arr)' });
 });
 
